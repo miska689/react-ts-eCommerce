8d94ac9979884318cb2d17511df51cefc18b607a
Mihail <mihaiboinceanu2004@gmail.com>
2024-10-11 16:21:51 +0300

    29 - lesson

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+const Footer: FunctionComponent = () => {
+    return (
+        <div>
+            Footer
+        </div>
+    );
+};
+
+export default Footer;
